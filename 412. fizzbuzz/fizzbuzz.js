a1981293ee533099e40b8f76043954324aa0eae1
DESKTOP-JD7FTGP\doduy <do.duythinh1@gmail.com>
2023-01-05 02:10:56 +0700

    add leet code 412

---
+++ 412. fizzbuzz/fizzbuzz.js
@@ -0,0 +1,14 @@
+var fizzBuzz = function(n) {
+     let output = [];
+     for(let i = 1 ; i <= n; i++){
+         let newString = '';
+         if(i % 3 === 0){
+             newString += "Fizz"
+         }
+         if(i % 5 === 0){
+             newString += "Buzz"
+         }
+         output.push(newString.length ? newString  : i.toString());
+     }
+     return output;
+};
