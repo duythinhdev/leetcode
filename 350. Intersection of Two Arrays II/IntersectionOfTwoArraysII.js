fb596e71cd2569d6bdf693054684c978472da857
DESKTOP-JD7FTGP\doduy <do.duythinh1@gmail.com>
2023-01-10 10:00:27 +0700

    Insection

---
+++ 350. Intersection of Two Arrays II/IntersectionOfTwoArraysII.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    const res = [];
+    for(let i = 0; i < nums2.length;i++){
+        if(nums1.includes(nums2[i])){
+            res.push(nums2[i]);
+            nums1.splice(nums1.indexOf(nums2[i]), 1);
+        }
+    }
+    return res;
+};
