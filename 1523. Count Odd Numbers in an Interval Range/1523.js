f7c2adb55cf350b94a56b2b81c474482106a1a24
DESKTOP-JD7FTGP\doduy <do.duythinh1@gmail.com>
2023-02-14 09:49:29 +0700

    add 1523

---
+++ 1523. Count Odd Numbers in an Interval Range/1523.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+  if (low % 2 === 0 && high % 2 === 0) {
+    console.log((high - low) / 2);
+    return (high - low) / 2;
+  } else {
+    return Math.floor((high - low) / 2) + 1;
+  }
+};
