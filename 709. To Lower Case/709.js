c47d31667f5a8be4408446e5ef5e1a76a5cf3aa0
MSI\ADMIN <do.duythinh1@gmail.com>
2023-04-09 22:14:52 +0700

    709 solution student

---
+++ 709. To Lower Case/709.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    // let newString = [];
+    // for(let i = 0 ;i < s.length; i++){
+    //     if(s[i] === s[i].toUpperCase()){
+    //         newString.push(s[i].split('').toLowerCase().join(''));
+    //     }
+    // }
+    return s.toLowerCase();
+};
