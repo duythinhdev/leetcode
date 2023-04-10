9de177fd394e66b2cbb00a610c1ebdb439e275a0
MSI\ADMIN <do.duythinh1@gmail.com>
2023-04-10 07:15:15 +0700

    709

---
+++ 709. To Lower Case/709solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    let output = '';
+    for (let i = 0; i < s.length; i++) {
+        const character = s.charCodeAt(i);
+        if (character >= 65 && character <= 90) {
+            output += String.fromCharCode(character + 32);
+        } else {
+            output += s[i];
+        }
+    }
+    
+    return output;
+};
