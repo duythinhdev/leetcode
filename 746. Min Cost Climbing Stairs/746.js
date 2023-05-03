fb4af90c977039e4432ae4df52423a6edd1b392e
MSI\ADMIN <do.duythinh1@gmail.com>
2023-05-03 13:30:26 +0700

    746

---
+++ 746. Min Cost Climbing Stairs/746.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let even = cost[0];
+    let odd = cost[1];
+    
+    for (let i = 2; i < cost.length; i++) {
+        if (i%2) {
+            odd = Math.min(odd + cost[i], even + cost[i]);
+        } else {
+            even = Math.min(even + cost[i], odd + cost[i]);
+        }
+    }
+    return Math.min(odd, even);
+};
