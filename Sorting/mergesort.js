456d664818bb1509c2159a28519bd9e2b62e6d3e
duythinhdev <do.duythinh1@gmail.com>
2024-02-11 23:56:36 +0700

    561

--- Sorting/mergesort.js
+++ Sorting/mergesort.js
@@ -7,21 +7,56 @@ function mergeSort(arr) {
   const middle = Math.floor(arr.length / 2);
   const leftHalf = arr.slice(0, middle);
   const rightHalf = arr.slice(middle);
-
   // Đệ quy để sắp xếp từng phần
+  console.log('leftHalf',leftHalf);
+  console.log('rightHalf',rightHalf);
   const sortedLeft = mergeSort(leftHalf);
+  console.log('sortedLeft',sortedLeft);
   const sortedRight = mergeSort(rightHalf);
+  console.log('sortedRight',sortedRight);
 
   // Hợp nhất hai mảng đã sắp xếp
   return merge(sortedLeft, sortedRight);
 }
-
+// function mergeSort(arr){
+//   if(arr.length <= 1){
+//     return arr;
+//   };
+//   const middle = Math.floor(arr.length / 2);
+//   const leftHalf = arr.slice(0,middle);
+//   const rightHalf = arr.slice(middle);
+//   const sortedLeft = mergeSort(leftHalf);
+//   const sortedRight = mergeSort(rightHalf);
+//   return merge(sortedLeft, sortedRight);
+// }
+// function merge(left,right){
+//   let result = [];
+//   let leftIndex = 0;
+//   let rightIndex = 0;
+//   while(leftIndex < left.length && rightIndex < right.length){
+//     if(left[leftIndex] < right[rightIndex]){
+//       result.push(left[leftIndex]);
+//       leftIndex++;
+//     }else {
+//       result.push(right[rightIndex]);
+//       rightIndex++;
+//     }
+//   }
+//   while (leftIndex < left.length){
+//     result.push(left[leftIndex]);
+//     leftIndex++;
+//   }
+//   while(rightIndex < right?.length){
+//     result.push(right[rightIndex]);
+//     rightIndex++;
+//   }
+//   return result;
+// }
 function merge(left, right) {
   let result = [];
   let leftIndex = 0;
   let rightIndex = 0;
 
-  // So sánh từng phần tử của hai mảng và hợp nhất theo thứ tự tăng dần
   while (leftIndex < left.length && rightIndex < right.length) {
     if (left[leftIndex] < right[rightIndex]) {
       result.push(left[leftIndex]);
