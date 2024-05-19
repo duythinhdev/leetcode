8cc79599aa8d0a4f1b1e27c376f65415598d0c2b
duythinhdev <do.duythinh1@gmail.com>
2024-05-19 14:27:54 +0700

    1768 use string

--- 1768. Merge Strings Alternately/1768.js
+++ 1768. Merge Strings Alternately/1768.js
@@ -3,12 +3,21 @@
  * @param {string} word2
  * @return {string}
  */
+// var mergeAlternately = function(word1, word2) {
+//     const newArray = [];
+//     const length = Math.max(word1.length, word2.length);
+//     for (let i = 0; i < length; i++) {
+//         if (word1[i]) newArray.push(word1[i]);
+//         if (word2[i]) newArray.push(word2[i]);
+//     }
+//     return newArray.join('');
+// };
+
 var mergeAlternately = function(word1, word2) {
-    const newArray = [];
-    const length = Math.max(word1.length, word2.length);
-    for (let i = 0; i < length; i++) {
-        if (word1[i]) newArray.push(word1[i]);
-        if (word2[i]) newArray.push(word2[i]);
+    let result = ''
+    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if (i < word1.length) result += word1[i]
+        if (i < word2.length) result += word2[i]
     }
-    return newArray.join('');
+    return result
 };
