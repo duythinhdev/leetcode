69d7e6e82f994e194650c53b6f41f4b24e7e071e
duythinhdev <do.duythinh1@gmail.com>
2024-10-09 00:08:55 +0700

    solution with  %/10 seprate to reversed

--- 9. Palindrome Number/9.js
+++ 9. Palindrome Number/9.js
@@ -2,16 +2,32 @@
  * @param {number} x
  * @return {boolean}
  */
- var isPalindrome = function(x) {
-    console.log(x.toString());
+var isPalindrome = function(x) {
+    // solution 1
     // let revered = x.toString().split('').reverse().join('');
     // console.log("revered",revered);
     // return (x.toString() === revered);
-    let strString = x.toString();
-    let sumString = ''
-    for (const element of strString) {
-        sumString +=element;
-        console.log("sumString",sumString);
+    // solution 2
+    // let strString = x.toString();
+    // let sumString = ''
+    // for (const element of strString) {
+    //     sumString += element;
+    // }
+    // return (sumString === strString)
+
+    // solution 3
+    if (x < 0) {
+        return false;
     }
-    return (sumString === strString)
+    let reverse = 0;
+    let xcopy = x;
+    while (x > 0) {
+        reverse = (reverse * 10) + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return reverse === xcopy;
 };
+
+const log = isPalindrome(121);
+
+console.log('isPalindrome', log);
