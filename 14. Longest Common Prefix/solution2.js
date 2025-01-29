c95bf1cb8686c4a54caed956d67fea9955a6f0bd
duythinhdev <do.duythinh1@gmail.com>
2025-01-29 18:24:14 +0700

    longest common prefix

---
+++ 14. Longest Common Prefix/solution2.js
@@ -0,0 +1,13 @@
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0];
+    for (let i = 0; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
+
+        }
+    }
+    return prefix;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
