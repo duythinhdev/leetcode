346f7b478abca995535f319e26b83e94ecd7ecb0
duythinhdev <do.duythinh1@gmail.com>
2025-03-11 20:22:18 +0700

    solution2 lsofc

---
+++ LSOfC/solution2.js
@@ -0,0 +1,22 @@
+function largestSubset(cards){
+    const countMap = new Map();
+    const numArrays = cards.length;
+    for(let arr of cards){
+        const uniqueSet = new Set(arr);
+        console.log('uniqueSet',uniqueSet);
+        for(let num of uniqueSet){
+            countMap.set(num, (countMap.get(num) || 0) + 1);
+        }
+    }
+    const commonElements = [...countMap.keys()].filter(num => countMap.get(num) === numArrays);
+    console.log('commonElements');
+    return cards[0]?.filter(num => commonElements.includes(num));
+}
+
+const arrays = [
+    [1, 3, 4, 6, 7, 9],
+    [1, 2, 4, 6, 8, 9],
+    [1, 4, 6, 9]
+];
+
+console.log(largestSubset(arrays)); // [1, 4, 6, 9]
