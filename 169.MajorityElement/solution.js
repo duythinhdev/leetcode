58c07efb5dbb4805b10a1e4d141bca572968f59a
duythinhdev <do.duythinh1@gmail.com>
2025-03-12 15:53:23 +0700

    learn solution 2

--- 169.MajorityElement/solution.js
+++ 169.MajorityElement/solution.js
@@ -1,14 +1,39 @@
-function majorityElement(nums) {
+// function majorityElement(nums) {
+//     const newMap = new Map();
+//     const length = nums.length / 2;
+//     for(let i = 0; i < nums.length; i++) {
+//         newMap.set(nums[i], (newMap.get(nums[i]) || 0) + 1);
+//         if(newMap.get(nums[i]) > length) {
+//             return nums[i];
+//         }
+//     }
+// }
+
+// function majorityElement(nums) {
+//     const newMap = new Map();
+//     const length = nums.length / 2;
+//     const result = [];
+//     for(let i = 0; i < nums.length; i++) {
+//         newMap.set(nums[i], (newMap.get(nums[i]) || 0) + 1);
+//         if(newMap.get(nums[i]) > length && !result.includes(nums[i])) {
+//             result.push(nums[i]);
+//         }
+//     }
+//     return result.join('');
+// }
+
+function majorityElement(nums){
     const newMap = new Map();
     const length = nums.length / 2;
-    for(let i = 0; i < nums.length; i++) {
+    const result = [];
+    for(let i = 0; i < nums.length; i ++){
         newMap.set(nums[i], (newMap.get(nums[i]) || 0) + 1);
-        if(newMap.get(nums[i]) > length) {
-            return nums[i];
+        if(newMap.get(nums[i]) > length && !result.includes(nums[i])){
+            result.push(nums[i]);
         }
     }
+    return result.join('');
 }
-
-const arrays = [3,2,3];
+const arrays = [2,2,1,1,1,2,2,2];
 
 console.log(majorityElement(arrays)); // [3,2,3]
