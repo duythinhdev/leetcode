7b2b907a1727a9719b58748855223a64f681e120
duythinhdev <do.duythinh1@gmail.com>
2025-04-01 15:26:27 +0700

    valid drome

--- 125. Valid Palindrome/ValidPalindrome.js
+++ 125. Valid Palindrome/ValidPalindrome.js
@@ -2,20 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+const isPalindrome = function(s) {
       // let re = /[\W_]/g;
       // let lowRegStr = s.toLowerCase().replace(re,'');
       // console.log("lowRegStr",lowRegStr);
       // let reverseStr = lowRegStr.split('').reverse().join('');
       // console.log("reverseStr",reverseStr);
       // return reverseStr === lowRegStr;
-     var re = /[^A-Za-z0-9]/g;
-     s = s.toLowerCase().replace(re, '');
-     var len = s.length;
-     for (var i = 0; i < len/2; i++) {
+    const re = /[^A-Za-z0-9]/g;
+     s = s?.toLowerCase()?.replace(re, '');
+    const len = s.length;
+     for (let i = 0; i < len/2; i++) {
        if (s[i] !== s[len - 1 - i]) {
            return false;
        }
      }
      return true;
 };
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
