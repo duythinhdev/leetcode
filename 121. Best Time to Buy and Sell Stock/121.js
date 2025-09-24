9a01975f084ea63566eb20cc676f50910b4bd49d
duythinhdev <do.duythinh1@gmail.com>
2025-09-24 07:48:21 +0700

    improve thinkking

--- 121. Best Time to Buy and Sell Stock/121.js
+++ 121. Best Time to Buy and Sell Stock/121.js
@@ -5,7 +5,7 @@
 var maxProfit = function(prices) {
     let result = 0;
     let min = prices[0];
-    for(let i = 1; i < prices.length; i++) {
+    for(let i = 0; i < prices.length; i++) {
         min = Math.min(prices[i], min);
         console.log('min',min);
         result = Math.max(result, prices[i] - min);
