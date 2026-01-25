2d2c2fec7b9322fde3e91879e7cf52615cbf4dca
duythinhdev <do.duythinh1@gmail.com>
2026-01-25 19:57:16 +0700

    49.js

---
+++ 49. GroupAnagrams/49.js
@@ -0,0 +1,16 @@
+var groupAnagrams = function (strs) {
+    const map = new Map();
+
+    for (const str of strs) {
+        const key = str.split('').sort().join('');
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+
+    return Array.from(map.values());
+};
+
+console.log('groupAnagrams',groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
