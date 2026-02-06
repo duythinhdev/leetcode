4bc69466295a2ac9c8910460a6de0a23ed0878d3
duythinhdev <do.duythinh1@gmail.com>
2026-02-06 10:34:51 +0700

    change all

--- 49. GroupAnagrams/49.js
+++ 49. GroupAnagrams/49.js
@@ -1,43 +1,24 @@
-// var groupAnagrams = function (strs) {
-//     const map = new Map();
-//
-//     for (const str of strs) {
-//         const key = str.split('').sort().join('');
-//         console.log('key', key);
-//
-//         if (!map.has(key)) {
-//             map.set(key, []);
-//         }
-//         map.get(key).push(str);
-//     }
-//
-//     return Array.from(map.values());
-// };
-
 // const groupAnagrams = (strs) => {
-//     const map = new Map();
-//     for(const str of strs) {
-//         const key = str.split('').sort().join('');
-//         if(!map.has(key)){
-//             map.set(key, []);
-//         }
-//         map.get(key).push(str);
-//     }
-//     return Array.from(map.values());
-// }
+//   const map = new Map();
+//   for(const str of strs){
+//       const key = str.split('').sort().join('');
+//       if(!map.has(key)){
+//           map.set(key, []);
+//       }
+//       map.get(key).push(str);
+//   }
+//   return Array.from(map.values());
+// };
 
 const groupAnagrams = (strs) => {
     const map = new Map();
-    for(const str of strs) {
-        const count = new Array(26).fill(0);
-        for(const char of str) {
-            count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
-        }
-        const key = count.join('#');
+    for(const str of strs){
+        const key = str.split('').sort().join('');
         if(!map.has(key)){
-            map.set(key, []);
+            map.set(key,[]);
         }
         map.get(key).push(str);
     }
+    return Array.from(map.values());
 };
 console.log('groupAnagrams',groupAnagrams(['eat','tea','tan','ate','nat','bat']));
