85f3c91b8df65d3768b240838f3252c9e2f9721c
duythinhdev <do.duythinh1@gmail.com>
2026-02-17 16:51:26 +0700

    fighting leetcode

--- 49. GroupAnagrams/49.js
+++ 49. GroupAnagrams/49.js
@@ -1,14 +1,3 @@
-// const groupAnagrams = (strs) => {
-//     const map = new Map();
-//     for(const str of strs){
-//         const key = str.split('').sort().join('');
-//         if(!map.has(key)){
-//             map.set(key,[]);
-//         }
-//         map.get(key).push(str);
-//     }
-//     return Array.from(map.values());
-// };
 
 const groupAnagrams = (strs) => {
     const map = new Map();
--- 90. Subsets II/90.SubsetsII.js
+++ 90. Subsets II/90.SubsetsII.js
@@ -1,20 +1,18 @@
-function subsetsWithDup(nums){
-    const result = []
-    nums.sort((a, b) => a - b)
-  
+const subsetsWithDup = (nums) => {
+    const result = [];
+    nums.sort((a, b) => a - b);
+
     const aux = (index, current) => {
-      result.push(current)
-      for (let i = index; i < nums.length; i++) {
-        if (i > index && nums[i] === nums[i - 1]) {
-          continue
+        result.push([...current]);
+        for(let i = index; i < nums.length; i ++){
+            if(i > index && nums[i] === nums[i -1]) continue;
+            current.push(nums[i]);
+            aux(i + 1, current);
+            current.pop();
         }
-        aux(i + 1, [...current, nums[i]])
-      }
     }
-    aux(0, [])
+    aux(0, []);
     return result
-}
+};
 
-
-const subset = subsetsWithDup([1,2,2,3,3]);
-console.log(subset);
+console.log(subsetsWithDup([1,2,2]));
