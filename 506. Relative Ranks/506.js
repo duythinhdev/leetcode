8833b2008fd98c362d289b98a8b917a3596007b9
duythinhdev <do.duythinh1@gmail.com>
2026-03-04 11:12:59 +0700

    clean

--- 506. Relative Ranks/506.js
+++ 506. Relative Ranks/506.js
@@ -17,34 +17,25 @@
 //     return score;
 // };
 
-// function findRelativeRanks(score) {
-//     const n = score.length;
-//     const sortedScores = [...score].sort((a, b) => b - a);
-//
-//     const rankMap = new Map();
-//
-//     for (let i = 0; i < n; i++) {
-//         if (i === 0)      rankMap.set(sortedScores[i], "Gold Medal");
-//         else if (i === 1) rankMap.set(sortedScores[i], "Silver Medal");
-//         else if (i === 2) rankMap.set(sortedScores[i], "Bronze Medal");
-//         else              rankMap.set(sortedScores[i], String(i + 1));
-//     }
-//
-//     return score.map(s => rankMap.get(s));
-// }
-
 function findRelativeRanks(score){
-    const n  = score.length;
     const sortedScores = [...score].sort((a, b) => b - a);
-
     const map = new Map();
-    for(let i = 0; i < n; i++){
-        if(i === 0) map.set(sortedScores[i], 'Gold Medal');
-        else if(i === 1) map.set(sortedScores[i], 'Silver Medal');
-        else if(i === 2) map.set(sortedScores[i], 'Bronze Medal');
-        else map.set(sortedScores[i], String(i + 1));
+    for(let i = 0; i < sortedScores.length; i++){
+        if(i === 0) {
+            map.set(sortedScores[i], "Gold Medal");
+            continue;
+        }
+        if(i === 1) {
+            map.set(sortedScores[i], "Silver Medal");
+            continue;
+        }
+        if(i === 2) {
+            map.set(sortedScores[i], "Bronze Medal");
+            continue;
+        }
+        map.set(sortedScores[i], (i + 1).toString());
     }
-    return score.map(s => map.get(s));
+    return sortedScores.map((item) => map.get(item));
 }
 
 console.log('findRelativeRanks',findRelativeRanks([10,3,8,9,4]));
